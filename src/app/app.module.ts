b38f166b778d3699e3e1125f114086ef44e170c7
Hieu Minh <nmhieu04091999@gmail.com>
2022-05-10 20:44:16 +0700

    learn property binding & learn event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './components/hello/hello.component';
 import { TestComponent } from './components/test/test.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, HelloComponent, TestComponent],
+  declarations: [AppComponent, HelloComponent, TestComponent, DashboardComponent, HomeComponent, AboutComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
