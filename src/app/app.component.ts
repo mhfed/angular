b6bc03b956e4ce55e8b8271bfb66aafefcd9506e
Hieu Minh <nmhieu04091999@gmail.com>
2022-05-11 23:38:12 +0700

    learn ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public name = 'Hieu Minh';
-  public age = 10;
+  public fruits = [
+    { name: 'Banana', price: 300, sale: 100 },
+    { name: 'Strawberry', price: 500, sale: 0 },
+    { name: 'Apple', price: 100, sale: 0 },
+    { name: 'Oranges', price: 200, sale: 400 },
+  ];
+  ngOnInit(): void {
+    console.log('Fruits', this.fruits);
+  }
 }
